2de71cd8696b620d50d6d03303c7483b548eec64
zNightKidz <402248867@qq.com>
2022-01-12 18:15:19 +0800

    导入需要的库

--- project/group.py
+++ project/group.py
@@ -14,11 +14,14 @@ with open(r'人名单.csv', encoding="utf-8") as file:
         if not os.path.isfile(filepath):
             with open(filepath,'w', encoding="utf-8") as f:
                 f.write('姓名，电话')
+                f.write('\n')
 
-        for item in csv_read:
-
-            print('循环次数：',number)
-
-            if item[0][-1:] ==str(number):
-                with open(filepath,'a', encoding="utf-8") as writefile:
+        # for item in csv_read:
+        #     print('循环次数：',number)
+        #     if item[0][-1:] ==str(number):
+        #         with open(filepath,'a', encoding="utf-8") as writefile:
+        #             csv.writer(writefile,delimiter='\t').writerow(item)
+        with open (filepath,'a',encoding="utf-8") as writefile:
+            for item in csv_read:
+                if item[0][-1:] == str(number):
                     csv.writer(writefile,delimiter='\t').writerow(item)
