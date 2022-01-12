dba47ad902015fb8537a2a5e2f12bae9e638416f
zNightKidz <402248867@qq.com>
2022-01-12 18:00:49 +0800

    导入需要的库

---
+++ project/group.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import csv
+import os
+
+if not os.path.exists(r'分组'):
+    os.mkdir('分组')
+with open(r'人名单.csv', encoding="utf-8") as file:
+    csv_read = csv.reader(file,delimiter='\t')
+
+    for number in range(0,10):
+        print('文件名称循环',number)
+        filepath = '分组/尾号'+str(number)+'.csv'
+        if not os.path.isfile(filepath):
+            with open(filepath,'w', encoding="utf-8") as f:
+                f.write('姓名，电话')
+
+        for item in csv_read:
+
+            print('循环次数：',number)
+
+            if item[0][-1:] ==str(number):
+                with open(filepath,'a', encoding="utf-8") as writefile:
+                    csv.writer(writefile,delimiter='\t').writerow(item)
